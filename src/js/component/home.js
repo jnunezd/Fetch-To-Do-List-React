971efde0f9bbf66f27eae22802e56785073c3b8b
Joaquin Nuñez <joaquin16602@gmail.com>
2019-12-27 22:24:45 +0000

    Fetch a ToDO List in React

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { isContext } from "vm";
 
 //create your first component
+
 export function Home() {
+	const [message, setMessage] = useState({
+		title: ""
+	});
+
+	function addMessage(e) {
+		if (e.target.value != "" && e.keyCode == 13) {
+			let obj = {
+				title: e.target.value
+			};
+			let mess = message.concat(obj);
+			setMessage(mess);
+			e.target.value = "";
+		}
+	}
+
+	function deleteMessage(i) {
+		let mess = message.slice();
+		mess.splice(i, 1);
+		setMessage(mess);
+	}
+
+	function showDelete(i) {
+		let icon = document.querySelector("#item" + i);
+		icon.classList.remove("hide");
+	}
+
+	function hideDelete(i) {
+		let icon = document.querySelector("#item" + i);
+		icon.classList.add("hide");
+	}
+
+	function getTodos() {
+		fetch("https://jsonplaceholder.typicode.com/todos/")
+			.then(response => response.json())
+			.then(json => {
+				setMessage(json);
+			});
+	}
+
+	useEffect(() => {
+		getTodos();
+	}, []);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card">
+			<div className="card-body">
+				<div className="form-group">
+					<input
+						type="text"
+						name=""
+						className="form-control"
+						placeholder="What needs to be done?"
+						onKeyDown={e => addMessage(e)}
+					/>
+				</div>
+				<ul className="list-group">
+					{message.length > 0 &&
+						message.map((item, i) => {
+							return (
+								<li
+									key={i}
+									className="list-group-item"
+									onMouseOver={() => showDelete(i)}
+									onMouseOut={() => hideDelete(i)}>
+									{item.title}{" "}
+									<i
+										id={"item" + i}
+										className="fa fa-trash float-right hide"
+										onClick={() => deleteMessage(i)}
+									/>
+								</li>
+							);
+						})}
+				</ul>
+				<p className="card-text">
+					{message.length > 0
+						? message.length + " items left."
+						: "You don't have To-do's"}
+				</p>
+			</div>
 		</div>
 	);
 }
